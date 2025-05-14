303dc9cbad2c150392341691ef79f50300bea638
AliAwela <ali123awela@gmail.com>
2025-05-14 11:50:16 +0300

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- StudentHomework.Application/obj/Debug/net8.0/StudentHomework.Application.AssemblyInfo.cs
+++ StudentHomework.Application/obj/Debug/net8.0/StudentHomework.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentHomework.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cec3bb505dba566c9f9dc6743c8e99ce08df3ef4")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentHomework.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentHomework.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- StudentHomework.Infrastructure/obj/Debug/net8.0/StudentHomework.Infrastructure.AssemblyInfo.cs
+++ StudentHomework.Infrastructure/obj/Debug/net8.0/StudentHomework.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentHomework.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cec3bb505dba566c9f9dc6743c8e99ce08df3ef4")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentHomework.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentHomework.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
